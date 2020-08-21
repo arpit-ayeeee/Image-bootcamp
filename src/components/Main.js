51e2689a60089ac011512eb09a44cedb513d265b
Arpit Jaiswal <arpitjaiswal282000@gmail.com>
2020-08-21 18:31:19 +0530

    Added mobile view

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,19 +8,23 @@ class Main extends Component{
         return(
             <div className="container">
                 <div className="row" id="mainrow">
-                    <div className="col-4 offset-4">
+                    <div className="col-2 col-sm-4">
+                    </div>
+                    <div className="col-8 col-sm-4">
                         <div className="button-big">
                             <Button id="btn">
                                 <Link to="/createtask" className="links font-weight-bold">ADD IMAGE</Link>
                             </Button>
                             <br />
                             <Button id="btn">
-                                <Link to="/scoretask" className="links font-weight-bold">SCORE IMAGE</Link>
+                                <Link to="/scoretask" className="links font-weight-bold">RATE IMAGE</Link>
                             </Button>
                             <Button id="btn">
                                 <Link to="/display" className="links font-weight-bold">FEEDS</Link>
                             </Button>
                         </div>
+                    </div>
+                    <div className="col-2 col-sm-4">
                     </div>
                 </div>
             </div>
